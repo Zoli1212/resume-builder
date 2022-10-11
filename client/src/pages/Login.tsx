f3308d0984ca02a05a6ac5eac5fbe35ec8de9b42
Your Name <you@example.com>
2022-10-11 13:52:01 +0200

    some changes tailwind

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -20,7 +20,7 @@ export const Login = () => {
   };
 
 
-  const register = () => {
+  const login = () => {
     const loginUserObj = {  email, password };
 
     console.log(loginUserObj);
@@ -31,8 +31,8 @@ export const Login = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-[400px] flex space-y-5 flex-col">
-        <h1 className="font-semibold text-3xl text-primary">Welcome To NIX CV - Builder</h1>
+      <div className="w-[400px] flex space-y-5 flex-col p-5 shadow-lg border border-gray-300">
+        <h1 className="font-semibold text-3xl text-primary">Welcome To CV - Builder</h1>
        
         <input type="text" className="py-1 px-3 border-2 border-gray-500 rounded focus:outline-none w-full" placeholder='email' name='email' value={email} onChange={onChange} />
         <input type="text" className="py-1 px-3 border-2 border-gray-500 rounded focus:outline-none w-full" placeholder='password' name='password' value={password} onChange={onChange}/>
@@ -40,7 +40,7 @@ export const Login = () => {
 
         <div className="flex justify-between items-center">
           <Link className="text-primary" to={'/register'}>Don't has an account? signUp here</Link>
-          <button className="py-1 px-5 text-white bg-primary" onClick={register}>LOGIN</button>
+          <button className="py-1 px-5 text-white bg-primary" onClick={login}>LOGIN</button>
         </div>
       </div>
     </div>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -31,8 +31,8 @@ export const Register = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-[400px] flex space-y-5 flex-col">
-        <h1 className="font-semibold text-3xl text-primary">Welcome To NIX CV - Builder</h1>
+      <div className="w-[400px] flex space-y-5 flex-col p-5 shadow-lg border border-gray-300">
+        <h1 className="font-semibold text-3xl text-primary">Welcome To CV - Builder</h1>
         <input type="text" className="py-1 px-3 border-2 border-gray-500 rounded focus:outline-none w-full" placeholder='name' name='name' value={name} onChange={onChange}/>
         <input type="text" className="py-1 px-3 border-2 border-gray-500 rounded focus:outline-none w-full" placeholder='email' name='email' value={email} onChange={onChange} />
         <input type="text" className="py-1 px-3 border-2 border-gray-500 rounded focus:outline-none w-full" placeholder='password' name='password' value={password} onChange={onChange}/>
