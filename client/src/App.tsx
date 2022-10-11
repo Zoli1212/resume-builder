b900f130cb826e1c9b88481a2539c425fea12ff4
Your Name <you@example.com>
2022-10-11 12:51:07 +0200

    routing basic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Suspense} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route} from 'react-router-dom'
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { NoMatch } from './pages/NoMatch';
 
 function App() {
   return (
-    <div className="App">
-      
-       <h1 className='text-5xl'>THE AVANGERS</h1>
-    </div>
+    <Routes>
+      <Route path={'/'} element={<Home />} />
+      <Route path={'login'} element={<Login />} />
+      <Route path={'register'} element={<Register />} />
+      <Route path={'*'} element={<NoMatch />} />
+    </Routes>
   );
 }
 
---
+++ client/src/pages/NoMatch.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NoMatch = () => {
+  return (
+    <div className='text-5xl'>Page Not Found 500</div>
+  )
+}
